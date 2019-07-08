12c960dc0acc663919894921c40645006a4e67f2
houguokun <hgk324@163.com>
2019-07-08 23:06:45 +0800

    reverse integer

---
+++ DataStructureAndAlgorithm/leetcode/easy/7_reverse_integer.py
@@ -0,0 +1,28 @@
+class Solution:
+    """
+    -2147483648  2147483647 1534236469
+    """
+
+    def reverse(self, x):
+        if x < -2147483648 or x > 2147483647:
+            return 0
+
+        num = abs(x)
+        size = len(str(num))
+        y = 0
+        for i in range(size):
+            a = num % 10
+            num = num // 10
+            y = y * 10 + a
+
+        if y < -2147483648 or y > 2147483647:
+            return 0
+
+        if x < 0:
+            return -y
+        else:
+            return y
+
+
+if __name__ == '__main__':
+    print(Solution().reverse(1534236469))
