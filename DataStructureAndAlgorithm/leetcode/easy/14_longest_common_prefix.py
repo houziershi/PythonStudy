cb060c2c1026b0a00761e60f46c9dd0bbe3c450c
houguokun@lazyaudio.com <Hou1234>
2019-07-16 09:25:09 +0800

    longest common prefix

--- DataStructureAndAlgorithm/leetcode/easy/14_longest_common_prefix.py
+++ DataStructureAndAlgorithm/leetcode/easy/14_longest_common_prefix.py
@@ -1,3 +1,18 @@
 class Solution:
     def longestCommonPrefix(self, strs):
-        pass
+        if strs is None or len(strs) == 0:
+            return ''
+        else:
+            x = strs[0]
+            result = ''
+
+            for i in range(len(x)):
+                if not all(i < len(y) and x[i] == y[i] for y in strs[1:]):
+                    break
+                else:
+                    result += x[i]
+            return result
+
+
+if __name__ == '__main__':
+    print(Solution().longestCommonPrefix(["flower", "flow", "fliwht"]))
