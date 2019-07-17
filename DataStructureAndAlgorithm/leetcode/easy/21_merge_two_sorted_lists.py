e24f50e8a4153347fe3356e6db64e822ae344614
houguokun@lazyaudio.com <Hou1234>
2019-07-17 14:35:08 +0800

    mergeTwoLists

--- DataStructureAndAlgorithm/leetcode/easy/21_merge_two_sorted_lists.py
+++ DataStructureAndAlgorithm/leetcode/easy/21_merge_two_sorted_lists.py
@@ -7,4 +7,30 @@ class ListNode:
 
 class Solution:
     def mergeTwoLists(self, l1: ListNode, l2: ListNode):
-        pass
+        head = ListNode(None)
+        tail = head
+
+        while l1 or l2:
+            if l1 and l2:
+                if l1.val < l2.val:
+                    tail.next = ListNode(l1.val)
+                    tail = tail.next
+                    l1 = l1.next
+                else:
+                    tail.next = ListNode(l2.val)
+                    tail = tail.next
+                    l2 = l2.next
+            elif l1:
+                tail.next = ListNode(l1.val)
+                tail = tail.next
+                l1 = l1.next
+            elif l2:
+                tail.next = ListNode(l2.val)
+                tail = tail.next
+                l2 = l2.next
+
+        return head.next
+
+
+if __name__ == '__main__':
+    print(Solution().mergeTwoLists(ListNode(1), ListNode(2)))
