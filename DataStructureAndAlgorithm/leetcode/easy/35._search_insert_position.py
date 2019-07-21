23649c7c2db3cf13c6a9421b7e16b1c29c100c5a
houguokun <hgk324@163.com>
2019-07-21 23:18:30 +0800

    search insert position

---
+++ DataStructureAndAlgorithm/leetcode/easy/35._search_insert_position.py
@@ -0,0 +1,36 @@
+class Solution:
+    def searchInsert(self, nums, target) -> int:
+        if target < nums[0]:
+            return 0
+        elif target > nums[len(nums) - 1]:
+            return len(nums)
+        return self.binarySearch(nums, 0, len(nums) - 1, target)
+
+    def binarySearch(self, nums, low, high, target):
+        if low > high:
+            return low
+        else:
+            middle = (low + high) // 2
+            if target > nums[middle]:
+                return self.binarySearch(nums, middle + 1, high, target)
+            elif target < nums[middle]:
+                return self.binarySearch(nums, low, middle - 1, target)
+            else:
+                return middle
+
+
+if __name__ == '__main__':
+    print(Solution().searchInsert([1, 3], 2))
+
+    """
+        Time Complexity = O(log(x))
+        Space Complexity = O(1)
+        Given a sorted array and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+        You may assume no duplicates in the array.
+        Example:
+        Input: [1,3,5,6], 5
+        Output: 2
+        
+        Input: [1,3,5,6], 2
+        Output: 1
+    """
