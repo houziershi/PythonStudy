1135f33f4e63f66a8c5fcd64136d787e4f4f1f14
houguokun <hgk324@163.com>
2020-05-05 21:26:00 +0800

    322动态规划

---
+++ DataStructureAndAlgorithm/leetcode/medium/322_coin_change.py
@@ -0,0 +1,26 @@
+import sys
+
+
+class Solution:
+    def coinChange(self, coins, amount: int) -> int:
+        cac_value = [0]
+
+        for i in range(1, amount + 1):
+            cac_value.append(min(cac_value[i - c] + 1 if i - c >= 0 else sys.maxsize for c in coins))
+
+        return -1 if cac_value[amount] >= sys.maxsize else cac_value[amount]
+
+
+if __name__ == '__main__':
+    print("......", Solution().coinChange([2], 3))
+
+    """
+        Time Complexity = O(N^2)
+        Space Complexity = O(N)
+        You are given coins of different denominations and a total amount of money amount. Write a function to compute the fewest number of coins that you need to make up that amount. 
+        If that amount of money cannot be made up by any combination of the coins, return -1.
+        Example:
+        Input: coins = [1, 2, 5], amount = 11
+        Output: 3 
+        Explanation: 11 = 5 + 5 + 1
+    """
